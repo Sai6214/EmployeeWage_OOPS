7f88e4c8713afbc5c2eebf1444ef7b9972ab098e
Saikumar <muthyalasaikumar9@gmail.com>
2022-05-07 20:24:46 +0530

    first comment

---
+++ EmployeeWage_OOPS/employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage_OOPS
+{
+    class Employee
+    {
+        public void Calculate()
+        {
+            int Is_Present = 1;
+            Random r = new Random();
+            int check = r.Next(0, 2);
+            if (Is_Present == check)
+                Console.WriteLine("Employee Is Present");
+            else
+                Console.WriteLine("Employee Is Absent");
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
