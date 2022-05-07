e970555e12f0bf70b4eb511106e9d2f21c72950a
Saikumar <muthyalasaikumar9@gmail.com>
2022-05-07 20:55:20 +0530

    fourth commit

--- EmployeeWage_OOPS/employee.cs
+++ EmployeeWage_OOPS/employee.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,26 +12,22 @@ namespace EmployeeWage_OOPS
     {
         public void Calculate()
         {
-            int Is_Present = 1;
+            int Is_Part_Time = 2;
+            int Is_Full_Time = 1;
             int Wage_Per_Hr = 20;
             int Daily_Wage = 0;
             int WorkingHrs = 0;
             Random r = new Random();
-            int check = r.Next(0, 2);
-            if (Is_Present == check)
-<<<<<<< HEAD
-                Console.WriteLine(" Employee Is Present");
-            else
-                Console.WriteLine("Employee Is Absent");
-            Console.ReadLine();
-=======
+            int check = r.Next(0, 3);
+            if (Is_Full_Time == check)
                 WorkingHrs = 8;
+            if (Is_Part_Time == check)
+                WorkingHrs = 4;
             else
                 WorkingHrs = 0;
             Daily_Wage = WorkingHrs + Wage_Per_Hr;
             Console.WriteLine(Daily_Wage);
->>>>>>> uc2
-             Console.ReadLine();
+            Console.ReadLine();
 
         }
     }
